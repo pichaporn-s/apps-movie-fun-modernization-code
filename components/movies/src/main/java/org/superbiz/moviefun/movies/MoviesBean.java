05222ada85fddca8b82097555da5bb48aa0e960a
Manoj Potla <potlamf@gmail.com>
2018-04-10 16:36:48 +0800

    completed multi-jars

--- components/movies/src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
+++ components/movies/src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
@@ -20,6 +20,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.superbiz.moviefun.movies.Movie;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
